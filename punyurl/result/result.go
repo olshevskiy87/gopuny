834320ab1f84a9d02deec0d872259cf25c52e2e0
Dmitriy Olshevskiy <olshevskiy87@bk.ru>
2018-03-15 22:41:18 +0300

    result: add method AsJSON()

--- punyurl/result/result.go
+++ punyurl/result/result.go
@@ -1,13 +1,16 @@
 package result
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+)
 
 // Result keeps shorten URL parts and original URL
 type Result struct {
-	ASCII   string `xml:"ascii"`
-	Preview string `xml:"preview"`
-	Puny    string `xml:"puny"`
-	URL     string `xml:"url"`
+	ASCII   string `xml:"ascii" json:"ascii"`
+	Preview string `xml:"preview" json:"preview"`
+	Puny    string `xml:"puny" json:"puny"`
+	URL     string `xml:"url" json:"url"`
 }
 
 func (r *Result) String() string {
@@ -18,4 +21,13 @@ func (r *Result) String() string {
 		"Preview", r.Preview,
 		"Original", r.URL,
 	)
+}
+
+// AsJSON returns Result struct encoded as json
+func (r *Result) AsJSON() (string, error) {
+	jsonText, err := json.Marshal(r)
+	if err != nil {
+		return "", err
+	}
+	return string(jsonText), nil
 }
